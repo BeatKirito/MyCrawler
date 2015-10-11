d0c4a982714e1152f024aa9929936c2c08337361
BeatKirito <beatkirito@gmail.com>
2015-10-11 16:46:28 +0800

    项目重新整理和修复

---
+++ src/com/beat/crawler/controller/TestServlet.java
@@ -0,0 +1,22 @@
+package com.beat.crawler.controller;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by beatkirito on 15-10-11.
+ */
+public class TestServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        doGet(request, response);
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        response.getWriter().println("Hello world!!!");
+    }
+}
